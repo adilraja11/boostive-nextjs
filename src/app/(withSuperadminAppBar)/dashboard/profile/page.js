af513a8dbde1276ca2806ca73504b531bbf0d233
adilraja11 <adilraja19@students.undip.ac.id>
2024-06-16 23:35:02 +0700

    fetching GET  API on Profile Dashboard Page

--- src/app/(withSuperadminAppBar)/dashboard/profile/page.js
+++ src/app/(withSuperadminAppBar)/dashboard/profile/page.js
@@ -1,6 +1,6 @@
 import { Profile } from "@/components/Profile";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="flex flex-col w-full max-w-5xl">
       <h1 className="text-2xl font-semibold mb-4">Profil</h1>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,50 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCamera, faLock } from '@fortawesome/free-solid-svg-icons'
+import { API_URL } from '@/config/apiUrl'
 
 export const Profile = () => {
+  const [profile, setProfile] = useState(null);
+
+  const [fullName, setFullName] = useState('');
+  const [gender,setGender] = useState('');
+  const [address, setAddress] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const profileData = localStorage.getItem("user");
+      if (profileData) {
+        const user = JSON.parse(profileData);
+  
+        try {
+          const response = await fetch(`${API_URL}/api/users/${user.id}`, {
+            cache: 'no-store'
+          });
+          
+          const {data} = await response.json();
+
+          setProfile(data);
+          setFullName(data.fullName);
+          setGender(data.gender);
+          setAddress(data.address);
+          setPhoneNumber(data.phoneNumber);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  if(!profile) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='bg-white shadow-sm shadow-gray-500 rounded-lg px-10 py-4 flex flex-col gap-4'>
       <div className="relative w-[150px]">
@@ -23,26 +62,26 @@ export const Profile = () => {
       </div>
       <div className="flex flex-col gap-2">
           <label>Nama Lengkap</label>
-          <input value={'Muhammad Adil Raja Saputra'}></input>
+          <input value={fullName}></input>
       </div>
       <div className="flex flex-col gap-2">
           <label>Jenis Kelamin</label>
-          <select className="border-2 rounded-lg px-3 py-2">
-              <option>Laki-laki</option>
-              <option>Perempuan</option>
+          <select value={gender} className="border-2 rounded-lg px-3 py-2">
+              <option value={'MALE'}>Laki-laki</option>
+              <option value={'FEMALE'}>Perempuan</option>
           </select>
       </div>
       <div className="flex flex-col gap-2">
           <label>Alamat</label>
-          <textarea className="border-2 rounded-lg px-3 py-2" value={'Komplek Dewa Kembar Blok A/8'} rows={'3'}></textarea>
+          <textarea className="border-2 rounded-lg px-3 py-2" value={address} rows={'3'}></textarea>
       </div>
       <div className="flex flex-col gap-2">
           <label>Nomor Telepon</label>
-          <input placeholder='08xx'></input>
+          <input value={phoneNumber} placeholder='08xx'></input>
       </div>
       <div className="flex flex-col gap-2">
           <label>Email</label>
-          <input value={'superadmin@gmail.com'} disabled></input>
+          <input value={profile.email} disabled></input>
           <div className="divider"></div>
           <a href="#" onClick={() => document.getElementById('change_password').showModal()} className="link link-hover text-blue-600 flex items-center gap-2">
             <FontAwesomeIcon icon={faLock}/>
