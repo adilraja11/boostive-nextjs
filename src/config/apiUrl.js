62a5cecac9149df42d3d6db5b319bbb69828e7f9
adilraja11 <adilraja19@students.undip.ac.id>
2024-06-22 21:58:34 +0700

    changed on API URL

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://localhost:3000";
+export const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://boostive-nextjs.vercel.app/";
